b02216a537400ee12099bf7218aa76ad6f18109a
GABRIEL YURI FERNANDES <107518044+Gabriel-Yuri-Fernandes@users.noreply.github.com>
2022-06-30 18:31:03 -0300

    Update InserirProduto1.py

--- InserirProduto1.py
+++ InserirProduto1.py
@@ -1,25 +1,26 @@
 from Grava_imagem import grava
 import mysql.connector
 
-host = 'localhost'
-database = 'para_teste'
-user = 'root'
-password = 'admin'
+host = ***
+database = ****
+user = ****
+password = ****
 
-conexao = mysql.connector.connect(host=host, database=database, user=user, password=password)
+conexao = mysql.connector.connect(host=host, database=database, user=user, password=password) #Criar uma conexao com o BD
 
-def inserir(idproduto, nome, descricao, estoque, valor, imagem=""):
+def inserir(idproduto, nome, descricao, estoque, valor, imagem=""): #Recebe as varaiveis que serao passadas ao chamar a função
     idproduto = idproduto
     nome = nome
     descricao = descricao
     estoque = estoque
     valor = valor
     imagem = imagem
-    if imagem != " ":
-        novodestino = grava(idproduto, imagem)
-    curso = conexao.cursor()
+    if imagem != " ": #Verificar se a variavel imagem não esta vazia
+        novodestino = grava(idproduto, imagem) #Caso não esteja vazia chama a função para copia a imagem em um novo destino
+    curso = conexao.cursor() #cria um cursor para trabalhar no BD
+    #abaixo cria uma variavel onde é passados todos os dados para o BD no formado especifico do banco
     dados = "INSERT INTO produto (idproduto, nome, descricao, estoque, preco, imagem) VALUES (" + idproduto + ',\'' + nome + '\',' + '\'' + descricao + '\',' + estoque +',' + valor + ',\'' + novodestino +'\');'
-    curso.execute(dados)
-    conexao.commit()
+    curso.execute(dados) #Executa no banco as informações passadas pela variavel
+    conexao.commit() #Confirmar a execução da funçao acima
 
-    curso.close()
+    curso.close() #Fecha o cursor
