45c75b936e599871aecb82e6b9e5dbe51231ae26
GABRIEL YURI FERNANDES <107518044+Gabriel-Yuri-Fernandes@users.noreply.github.com>
2022-06-30 18:42:42 -0300

    Update Busca_Imagem.py

--- Busca_Imagem.py
+++ Busca_Imagem.py
@@ -1,8 +1,8 @@
 from tkinter.filedialog import askopenfilename
 
 
-def imagem():
+def imagem(): # Comando para fará a busca do local de uma imagem ou aquivo
     filename = askopenfilename()
 
-    return filename
+    return filename #retorna o local que o arquivo estava e até o proprio arquivo
 
