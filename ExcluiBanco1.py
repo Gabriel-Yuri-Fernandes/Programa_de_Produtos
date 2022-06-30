17f5bedb3d6f71cf7fa48c1cf64169c9b11ac733
GABRIEL YURI FERNANDES <107518044+Gabriel-Yuri-Fernandes@users.noreply.github.com>
2022-06-30 18:39:00 -0300

    Update ExcluiBanco1.py

--- ExcluiBanco1.py
+++ ExcluiBanco1.py
@@ -1,23 +1,23 @@
 import os
 import mysql.connector
 
-host = 'localhost'
-database = 'para_teste'
-user = 'root'
-password = 'admin'
+host = '****'
+database = '****'
+user = '***'
+password = '****'
 
-conexao = mysql.connector.connect(host=host, database=database, user=user, password=password)
+conexao = mysql.connector.connect(host=host, database=database, user=user, password=password) #Cria conexão com o BD
 
 
-def Excluir(idproduto, endimg):
+def Excluir(idproduto, endimg): #Função que irá excluir o produto do BD
     end = endimg
     idproduto = idproduto
-    dados = "DELETE FROM produto WHERE idproduto = %d" % idproduto
-    curso = conexao.cursor(dados)
-    curso.execute(dados)
-    conexao.commit()
+    dados = "DELETE FROM produto WHERE idproduto = %d" % idproduto #Comando para excluir o item do BD, no formado que o BD aceita
+    curso = conexao.cursor()#Cria um cursor para navegar e editar no BD
+    curso.execute(dados) #Executa o comando salvo na variavel dado
+    conexao.commit() #Confirma a execução do comando acima
     os.remove(end)
 
-    curso.close()
+    curso.close() #Fecha o cursor
 
 
